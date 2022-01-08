dd318c7fcd8e0e65d505ac030d1aa23e11121436
Naomi-Rhames <naomirhames2020@gmail.com>
2022-01-07 22:48:08 -0600

    search feature

--- src/components/SearchTweet.js
+++ src/components/SearchTweet.js
@@ -1,23 +1,28 @@
-// import  {useState} from 'react'
+import  {useState} from 'react'
+import { TweetShow } from '.'
+import { connect } from 'react-redux'
+
+const SearchTweet = ({tweets}) => {
+    console.log(tweets)
+
+    const [getTweet, setSearch] = useState("")
 
 
-const SearchTweet = () => {
-  
-
-    // const [getTweet, setSearch] = useState("")
-
+    const handleChange = (e) => {
+        setSearch(e.target.value)
+    }
 
     return(
         <div>
             <form>
-                <input type='text' placeholder='Search Tweet'  /> 
+                <input type='text' placeholder='Search Tweet' onChange={handleChange} /> 
                 <input type='submit'/>
             </form>
-          
+           
          </div>
     )
   }
 
+const mapStateToProps = (state) => ({tweets: state.tweets })
 
-
-export default SearchTweet
+export default connect(mapStateToProps)(SearchTweet)
