2b0f943e03adeeaabe08ba4005e01f9a82f678a3
Naomi-Rhames <naomirhames2020@gmail.com>
2022-01-08 21:13:40 -0600

    Finishing Search

--- src/components/SearchTweet.js
+++ src/components/SearchTweet.js
@@ -18,7 +18,15 @@ const SearchTweet = ({tweets}) => {
                 <input type='text' placeholder='Search Tweet' onChange={handleChange} /> 
                 <input type='submit'/>
             </form>
-           
+            {tweets.filter(tweet => {
+                if(getTweet === ""){
+                    return tweet
+                } else if (tweet.description.toLowerCase().includes(getTweet.toLowerCase())){
+                    return tweet
+                }
+            }).map(tweet => 
+                <TweetShow  username={tweet.username} description={tweet.description} image_url={tweet.image_url} id={tweet.id} /> )
+                }
          </div>
     )
   }
